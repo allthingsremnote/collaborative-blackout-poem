930ba57f809b5be02a686b7bcff6b4933ce26090
allthingsremnote <allthingsremnote@gmail.com>
2021-05-05 23:47:24 -0400

    changed port

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ var colorDif = 50;
 function randomColor() {
     return "#" + Math.round(Math.random() * colorDif + colorMin) + Math.round(Math.random() * colorDif + colorMin) + Math.round(Math.random() * colorDif + colorMin);
 }
-http.listen(8080, () => {
+http.listen(80, () => {
     console.log('listening on *:8080');
 });
 return
